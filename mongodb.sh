2543b8898b5da6a93ba0d711585e5ab4492a6bd5
Ashish <ashishkumaryandra@gmail.com>
2026-02-24 21:14:19 +0530

    Instance creation automation

---
+++ mongodb.sh
@@ -0,0 +1,55 @@
+#!/bin/bash
+
+SG_ID="sg-0d5d4fedeb7568090"
+AMI_ID="ami-0220d79f3f480ecf5"
+ZONE_ID="Z06798631LYJMO4Z0P7UU"
+DOMAIN_NAME="devzone.fun"
+
+for instance in $@
+do 
+    INSTANCE_ID=$( aws ec2 run instances \
+    --image-id $AMI_ID \
+    --instance-type t3.micro \
+    --security-group-ids $SG_ID \
+    --tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value=$instance}]" \
+    --query "Instances[0].InstanceId" \
+    --output text )
+    
+    if [ $instance == "frontend" ]; then
+         IP=$(
+            aws ec2 describe-instances \
+            --instance-ids $INSTANCE_ID \
+            --query "Reservations[0].Instances[0].publicIpAddress" \
+            --output text )
+        RECORD_NAME="$DOMAIN_NAME"
+    else
+         IP=$(
+            aws ec2 describe-instances \
+            --instances-ids $INSTANCE_ID \
+            --query "Reservations[0].Instances[0].privateIpAddress" \
+            --output text )
+         RECORD_NAME="$instance.$DOMAIN_NAME"
+    fi
+echo "IP:$IP"
+aws route53 change-resource-record-sets \
+--hosted-zone-id $ZONE_ID \
+--change-batch '{
+    "Changes": [
+        {
+            "Action": "UPSERT",
+            "ResourceRecordSet": {
+                "Name": "'$RECORD_NAME'",
+                "Type": "A",
+                "TTL": 1,
+                "ResourceRecords": [
+                    {
+                        "Value": "'$IP'"
+                    }
+                ]
+            }
+        }
+    ]
+}'
+
+    echo records updated for $instance
+done
--- roboshop.sh
+++ roboshop.sh
@@ -1,55 +1,61 @@
-#!/bin/bash
-
 SG_ID="sg-0d5d4fedeb7568090"
 AMI_ID="ami-0220d79f3f480ecf5"
 ZONE_ID="Z06798631LYJMO4Z0P7UU"
 DOMAIN_NAME="devzone.fun"
 
 for instance in $@
-do 
-    INSTANCE_ID=$( aws ec2 run instances \
+do
+    INSTANCE_ID=$( aws ec2 run-instances \
     --image-id $AMI_ID \
-    --instance-type t3.micro \
+    --instance-type "t3.micro" \
     --security-group-ids $SG_ID \
     --tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value=$instance}]" \
-    --query "Instances[0].InstanceId" \
+    --query 'Instances[0].InstanceId' \
     --output text )
-    
+
     if [ $instance == "frontend" ]; then
-         IP=$(
+        IP=$(
             aws ec2 describe-instances \
             --instance-ids $INSTANCE_ID \
-            --query "Reservations[0].Instances[0].publicIpAddress" \
-            --output text )
-        RECORD_NAME="$DOMAIN_NAME"
+            --query 'Reservations[].Instances[].PublicIpAddress' \
+            --output text
+        )
+        RECORD_NAME="$DOMAIN_NAME" # daws88s.online
     else
-         IP=$(
+        IP=$(
             aws ec2 describe-instances \
-            --instances-ids $INSTANCE_ID \
-            --query "Reservations[0].Instances[0].privateIpAddress" \
-            --output text )
-         RECORD_NAME="$instance.$DOMAIN_NAME"
+            --instance-ids $INSTANCE_ID \
+            --query 'Reservations[].Instances[].PrivateIpAddress' \
+            --output text
+        )
+        RECORD_NAME="$instance.$DOMAIN_NAME" # mongodb.daws88s.online
     fi
-echo "IP:$IP"
-aws route53 change-resource-record-sets \
---hosted-zone-id $ZONE_ID \
---change-batch '{
-    "Changes": [
-        {
+
+    echo "IP Address: $IP"
+
+    aws route53 change-resource-record-sets \
+    --hosted-zone-id $ZONE_ID \
+    --change-batch '
+    {
+        "Comment": "Updating record",
+        "Changes": [
+            {
             "Action": "UPSERT",
             "ResourceRecordSet": {
                 "Name": "'$RECORD_NAME'",
                 "Type": "A",
                 "TTL": 1,
                 "ResourceRecords": [
-                    {
-                        "Value": "'$IP'"
-                    }
+                {
+                    "Value": "'$IP'"
+                }
                 ]
             }
-        }
-    ]
-}'
+            }
+        ]
+    }
+    '
 
-    echo records updated for $instance
+    echo "record updated for $instance"
+
 done
